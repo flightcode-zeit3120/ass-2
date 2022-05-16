c49283e494f99cf5f1a89e44b393ed356980b198
flightcode <flightcode@pm.me>
2022-05-16 11:19:49 +1000

    Add index placeholder

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const server = express();
+
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => console.log(`Database connected successfully`))
+  .catch((err) => console.error(err));
+
+mongoose.Promise = global.Promise;
+
+server.use(cors());
+server.use(express.json());
+server.get("/", (req, res) => {
+  res.status(200).send("Server Root");
+});
+
+// server.use("/api/...", ...);
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`Server is running on port ${port}`));
