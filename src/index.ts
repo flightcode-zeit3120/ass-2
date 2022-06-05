ed1a673e4a371f26679c47da48b1959524f69433
flightcode <flightcode@pm.me>
2022-06-05 15:53:28 +1000

    Add rate limiter

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import sanitize from "express-mongo-sanitize";
+import rateLimit from "express-rate-limit";
 import User from "./routes/user";
 import Warehouse from "./routes/warehouseItems";
+
 
 const server = express();
 
@@ -19,6 +21,16 @@ mongoose.Promise = global.Promise;
 server.use(cors());
 server.use(express.json());
 server.use(sanitize());
+
+// Add rate limiter to all API calls
+const limiter = rateLimit({
+	windowMs: 1000, // 1 second
+	max: 10, // Limit each IP to 10 requests per `window`
+	standardHeaders: true,
+	legacyHeaders: false,
+});
+server.use(limiter);
+
 server.get("/", (req, res) => {
   res.status(200).send("Server Root");
 });
